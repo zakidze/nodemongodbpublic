188e57e04c63beccd8e75cea14727e3796264742
zakidze <zakidze@gmail.com>
2022-03-15 20:28:35 +0100

    dfsq

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.sendFile("./index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./about.html", { root: __dirname });
+  res.render("about");
 });
 
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 app.use((req, res) => {
-  res.sendFile("./404.html", { root: __dirname });
+  res.render("404");
 });
 
 app.listen(3000);
